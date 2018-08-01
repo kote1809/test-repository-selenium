962d816e965285ecaec2befe81edf57745183a76
kote1809 <41379676+kote1809@users.noreply.github.com>
2018-08-01 14:32:57 +0300

    click menu

--- test.py
+++ test.py
@@ -9,10 +9,31 @@ def driver(request):
     request.addfinalizer(wd.quit)
     return wd
 
-def test_example(driver):
+def test_menu(driver):
+    #авторизация в админке
     driver.get("http://localhost/litecart/admin/")
     driver.find_element_by_name("username").send_keys("admin")
     driver.find_element_by_name("password").send_keys("admin")
     driver.find_element_by_name("remember_me").click()
     driver.find_element_by_name("login").click()
-    WebDriverWait(driver, 10).until(EC.title_is("My Store"))
+
+    #поиск всех элементов основного меню
+    menu = driver.find_elements_by_xpath("//ul[@id='box-apps-menu']//li[@id='app-']")
+    #проходим по всем элементам меню, кликаем на каждый пункт и проверяем, что на загруженной странице есть заголовок (h1)
+    for i in range(len(menu)):
+        menu[i].click()
+        h1 = driver.find_elements_by_xpath("//h1")
+        if len(h1) == 0:
+            print("Page hasn't elements h1")
+        #проверяем, есть ли вложенные элементы у текущего пункта меню
+        second = driver.find_elements_by_xpath("//li[@id='app-' and contains(@class, 'selected')]//li")
+        if len(second) > 0:
+            for k in range(len(second)):
+                second[k].click()
+                h1 = driver.find_elements_by_xpath("//h1")
+                if len(h1) == 0:
+                    print("Page hasn't elements h1")
+                #переопределаем список вложенных элементов
+                second = driver.find_elements_by_xpath("//li[@id='app-' and contains(@class, 'selected')]//li")
+        #переопределаем список элементов основного меню
+        menu = driver.find_elements_by_xpath("//ul[@id='box-apps-menu']//li[@id='app-']")
